b72d8cc9cf46e5ed517825db76f52e717a59c01b
zharr <zach.harrison55@gmail.com>
2019-11-23 11:44:05 -0500

    added mnist example

--- train_unet_tpu.py
+++ train_unet_tpu.py
@@ -1,45 +1,55 @@
-import numpy as np
 import os
-import schedulers
 from statistics import mean
+import shutil
+import time
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.utils.tensorboard import SummaryWriter
-import torchvision
-import torchvision.transforms as transforms
+from torchvision import datasets, transforms
 import torch_xla
-import torch_xla.debug.metrics as met
 import torch_xla.distributed.data_parallel as dp
+import torch_xla.debug.metrics as met
 import torch_xla.utils.utils as xu
 import torch_xla.core.xla_model as xm
-import unittest
+import torch_xla.test.test_utils as test_utils
 from unet_model import UNet
 from satelliteLoader import satelliteDataSet
-from torch.utils.data.sampler import SubsetRandomSampler
 
-lr = 0.0001
-lr_scheduler_divide_every_n_epochs = 20
-lr_scheduler_type = 'WarmupAndExponentialDecayScheduler'
-lr_scheduler_divisor = 5
-momentum=None
-num_cores = 8
-batch_size = 64
-test_set_batch_size = 128
-num_epochs = 10
-num_workers = 64
-valid_size = 0.1
-shuffle = True
-log_steps = 2
 datadir = '../data'
+logdir = '../logs'
+batch_size = 128
+momentum = 0.5
+lr = 0.01
+num_epochs = 18
+num_cores = 8
+num_workers = 12
+drop_last = True
+log_steps = 10
+metrics_debug = True
+
 
 def train_unet():
-  print('==> Preparing data..')
+  torch.manual_seed(1)
+
+  '''
+  train_dataset = datasets.MNIST(
+      datadir,
+      train=True,
+      download=True,
+      transform=transforms.Compose(
+          [transforms.ToTensor(),
+           transforms.Normalize((0.1307,), (0.3081,))]))
+  train_dataset_len = len(train_dataset)
+  test_dataset = datasets.MNIST(
+      datadir,
+      train=False,
+      transform=transforms.Compose(
+          [transforms.ToTensor(),
+           transforms.Normalize((0.1307,), (0.3081,))]))
+  '''
+
   img_dim = 1024
-
-
-
   normalize = transforms.Normalize(
       mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
@@ -80,71 +90,61 @@ def train_unet():
       train_dataset,
       batch_size=batch_size,
       sampler=train_sampler,
+      drop_last=drop_last,
       shuffle=False if train_sampler else True,
       num_workers=num_workers)
   test_loader = torch.utils.data.DataLoader(
       test_dataset,
-      batch_size=test_set_batch_size,
+      batch_size=batch_size,
       sampler=test_sampler,
+      drop_last=drop_last,
       shuffle=False,
       num_workers=num_workers)
 
-  torch.manual_seed(42)
-
-  devices = (xm.get_xla_supported_devices(max_devices=num_cores))
-
-  torchvision_model = UNet()
-  model_parallel = dp.DataParallel(torchvision_model, device_ids=devices)
+  devices = (
+      xm.get_xla_supported_devices(
+          max_devices=num_cores) if num_cores != 0 else [])
+  # Scale learning rate to num cores
+  lr = lr * max(len(devices), 1)
+  # Pass [] as device_ids to run using the PyTorch/CPU engine.
+  model_parallel = dp.DataParallel(UNet, device_ids=devices)
 
   def train_loop_fn(model, loader, device, context):
-    loss_fn = nn.CrossEntropyLoss()
+    loss_fn = nn.NLLLoss()
     optimizer = context.getattr_or(
-        'optimizer', lambda: optim.SGD(
-            model.parameters(),
-            lr=lr,
-            momentum=momentum,
-            weight_decay=1e-4))
-    lr_scheduler = context.getattr_or(
-        'lr_scheduler', lambda: schedulers.wrap_optimizer_with_scheduler(
-            optimizer,
-            scheduler_type=lr_scheduler_type,
-            scheduler_divisor=lr_scheduler_divisor,
-            scheduler_divide_every_n_epochs=lr_scheduler_divide_every_n_epochs,
-            num_steps_per_epoch=num_training_steps_per_epoch,
-            summary_writer=None))
+        'optimizer',
+        lambda: optim.SGD(model.parameters(), lr=lr, momentum=momentum))
     tracker = xm.RateTracker()
+
     model.train()
-    for x, (data, target) in loader:
+    for x, (data, target) in enumerate(loader):
       optimizer.zero_grad()
-      data = data.permute(0,3,1,2)
       output = model(data)
-      print('passed through model')
       loss = loss_fn(output, target)
       loss.backward()
       xm.optimizer_step(optimizer)
       tracker.add(batch_size)
       if x % log_steps == 0:
-        print('device: {}, x: {}, loss: {}, tracker: {}, tracker_global: {} '.format(device, x, loss.item(),
-                                                                                     tracker.rate(), tracker.global_rate()))
-      if lr_scheduler:
-        lr_scheduler.step()
+        test_utils.print_training_update(device, x, loss.item(),
+                                         tracker.rate(),
+                                         tracker.global_rate())
 
   def test_loop_fn(model, loader, device, context):
     total_samples = 0
     correct = 0
     model.eval()
-    for x, (data, target) in loader:
+    for data, target in loader:
       output = model(data)
       pred = output.max(1, keepdim=True)[1]
       correct += pred.eq(target.view_as(pred)).sum().item()
       total_samples += data.size()[0]
 
     accuracy = 100.0 * correct / total_samples
-    #test_utils.print_test_update(device, accuracy)
-    print('device: {}, accuracy: {}'.format(device, accuracy))
+    test_utils.print_test_update(device, accuracy)
     return accuracy
 
   accuracy = 0.0
+  writer = test_utils.get_summary_writer(logdir)
   num_devices = len(
       xm.xla_replication_devices(devices)) if len(devices) > 1 else 1
   num_training_steps_per_epoch = train_dataset_len // (
@@ -155,12 +155,16 @@ def train_unet():
     accuracy = mean(accuracies)
     print('Epoch: {}, Mean Accuracy: {:.2f}%'.format(epoch, accuracy))
     global_step = (epoch - 1) * num_training_steps_per_epoch
-    print('global step: {}'.format(global_step))
+    test_utils.add_scalar_to_summary(writer, 'Accuracy/test', accuracy,
+                                     global_step)
+    if metrics_debug:
+      print(met.metrics_report())
 
+  test_utils.close_summary_writer(writer)
   return accuracy
 
 
 
-
+# Run the tests.
 torch.set_default_tensor_type('torch.FloatTensor')
 train_unet()
